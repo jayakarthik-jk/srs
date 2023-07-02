902b7db831f20177e524c7701034a194ae95828b
Jayakarthik <98959858+jayakarthik-jk@users.noreply.github.com>
2023-07-03 01:19:15 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use srs::{Route, Server};
+use srs::{Request, Response};
 
 fn main() {
     let mut server = Server::new();
     let mut home_route = Route::new("/");
+    
     home_route
-        .get(|req, mut res| {
-            println!("got get {:?}", req.query);
-            res.status(200).send("hello get request");
-        })
+        .get(handler)
         .post(|req, mut res| {
             println!("got post {:?}", req.body);
             res.status(200).send("hello post request");
         });
 
     server.use_route(home_route);
-
+    // add your own routes
     server.listen("localhost:3000");
+}
+
+fn handler(req: Request, mut res: Response) {
+    println!("got get {:?}", req.query);
+    res.status(200).send("hello get request");
 }
